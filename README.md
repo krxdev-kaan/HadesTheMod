2014c59abde32a67b6e1a95887c2afa9eef719cc
krxdev-emir <emiregeerenbay@gmail.com>
2020-11-29 23:23:26 +0300

    Added items & roadmap

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # HadesTheMod
 A Fanmade mod that implements Hades's main aspects into Minecraft.
+## Roadmap
+![-](Roadmap.png)
 ## Authors
 - [KRXDevelops-Kaan](https://github.com/krxdev-kaan/)
 - [KRXDevelops-Emir](https://github.com/krxdev-emir/)
--- src/main/java/com/krxdevelops/hadesmod/init/ItemInit.java
+++ src/main/java/com/krxdevelops/hadesmod/init/ItemInit.java
@@ -10,5 +10,9 @@ public class ItemInit
 {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    public static final Item stygianMatter = new ItemBase("ore_StygianMatter");
+    public static final Item stygianMatter = new ItemBase("ore_Stygian");
+    public static final Item stygianIngot = new ItemBase("ingot_Stygian");
+    public static final Item darkness = new ItemBase("darkness");
+    public static final Item chthonicKey = new ItemBase("chtonicKey");
+    public static final Item titanblood = new ItemBase("titanblood");
 }
