21b9ecf602c5401a906cc94e9a2e1f8f7e819840
krxdev-kaan <kaanuyumazpre86@gmail.com>
2021-01-03 01:15:54 +0300

    Twin Fists Special WorkInProgress

--- src/main/java/com/krxdevelops/hadesmod/items/TwinFists.java
+++ src/main/java/com/krxdevelops/hadesmod/items/TwinFists.java
@@ -2,13 +2,28 @@ package com.krxdevelops.hadesmod.items;
 
 import com.google.common.collect.Multimap;
 import com.krxdevelops.hadesmod.HadesMod;
+import com.krxdevelops.hadesmod.capabilities.aegis.CapabilityAegis;
+import com.krxdevelops.hadesmod.capabilities.aegis.IAegis;
 import com.krxdevelops.hadesmod.init.ItemInit;
 import com.krxdevelops.hadesmod.util.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TwinFists extends Item implements IHasModel
 {
@@ -31,6 +46,50 @@ public class TwinFists extends Item implements IHasModel
     public float getAttackDamage()
     {
         return this.attackDamage;
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        if (handIn != EnumHand.MAIN_HAND)
+            return new ActionResult<ItemStack>(EnumActionResult.FAIL, stack);
+
+        List<Entity> entities = worldIn.getEntitiesWithinAABBExcludingEntity(playerIn, new AxisAlignedBB((int)Math.ceil(playerIn.posX)-1 - 4,
+                                                                                                        (int)Math.ceil(playerIn.posY)-1,
+                                                                                                        (int)Math.ceil(playerIn.posZ)-1 - 4,
+                                                                                                        (int)Math.ceil(playerIn.posX)-1 + 4,
+                                                                                                        (int)Math.ceil(playerIn.posY)+1,
+                                                                                                        (int)Math.ceil(playerIn.posZ)-1 + 4));
+
+        for (Entity e : entities)
+        {
+            e.addVelocity(0, 1.2D, 0);
+        }
+        playerIn.addVelocity(0, 0.8D, 0);
+
+        Iterable<BlockPos> iterable = BlockPos.getAllInBox( (int)Math.ceil(playerIn.posX)-1 - 4,
+                                                            (int)Math.ceil(playerIn.posY),
+                                                            (int)Math.ceil(playerIn.posZ)-1 - 4,
+                                                            (int)Math.ceil(playerIn.posX)-1 + 4,
+                                                            (int)Math.ceil(playerIn.posY),
+                                                            (int)Math.ceil(playerIn.posZ)-1 + 4);
+
+        for (BlockPos bp : iterable)
+        {
+            if (worldIn.getBlockState(bp).getBlock().isPassable(worldIn, bp))
+            {
+                worldIn.spawnParticle(EnumParticleTypes.CLOUD,
+                                        bp.getX(),
+                                        bp.getY(),
+                                        bp.getZ(),
+                                        0.0,
+                                        0.0,
+                                        0.0,
+                                        2);
+            }
+        }
+
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
     }
 
     public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
