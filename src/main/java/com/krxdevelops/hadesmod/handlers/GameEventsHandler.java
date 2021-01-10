8f3c2976ea7b2042e63ed981d6d6c3374e6b29e5
krxdev-kaan <kaanuyumazpre86@gmail.com>
2021-01-10 23:55:29 +0300

    FOV Event (TO-DO)

--- src/main/java/com/krxdevelops/hadesmod/handlers/GameEventsHandler.java
+++ src/main/java/com/krxdevelops/hadesmod/handlers/GameEventsHandler.java
@@ -1,12 +1,16 @@
 package com.krxdevelops.hadesmod.handlers;
 
+import com.krxdevelops.hadesmod.init.ItemInit;
 import com.krxdevelops.hadesmod.items.EternalSpearRecoverItem;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.FOVUpdateEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class GameEventsHandler
@@ -27,4 +31,19 @@ public class GameEventsHandler
             }
         }
     } */
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void onFOVUpdate(FOVUpdateEvent event)
+    {
+        ItemStack activeStack = event.getEntity().getActiveItemStack();
+        if (activeStack.getItem() == ItemInit.heartSeekingBow)
+        {
+            //event.setNewfov(event.getFov() * 1.0F);
+        }
+        else if (activeStack.getItem() == ItemInit.adamantRail)
+        {
+            //event.setNewfov(event.getFov() * 1.0F);
+        }
+    }
 }
