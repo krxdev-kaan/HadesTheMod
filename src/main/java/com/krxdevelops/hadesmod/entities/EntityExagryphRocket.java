df81c71258ffc409c4a0ea07bbf9dce5634edae6
krxdev-kaan <kaanuyumazpre86@gmail.com>
2021-01-18 10:46:54 +0300

    Exagryph Rocket Entity Implemented

---
+++ src/main/java/com/krxdevelops/hadesmod/entities/EntityExagryphRocket.java
@@ -0,0 +1,230 @@
+package com.krxdevelops.hadesmod.entities;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.*;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+
+import java.util.List;
+
+public class EntityExagryphRocket extends EntityThrowable
+{
+    private int xTile;
+    private int yTile;
+    private int zTile;
+    private Block inTile;
+    private int inData;
+    private int ticksInAir;
+    private int ticksInGround;
+
+    public EntityExagryphRocket(World worldIn)
+    {
+        super(worldIn);
+    }
+
+    public EntityExagryphRocket(World worldIn, double x, double y, double z)
+    {
+        super(worldIn, x ,y, z);
+    }
+
+    public EntityExagryphRocket(World worldIn, EntityLivingBase throwerIn)
+    {
+        super(worldIn, throwerIn);
+    }
+
+    protected float getGravityVelocity()
+    {
+        return 0.01F;
+    }
+
+    public void onUpdate()
+    {
+        this.lastTickPosX = this.posX;
+        this.lastTickPosY = this.posY;
+        this.lastTickPosZ = this.posZ;
+
+        if (this.throwableShake > 0)
+        {
+            --this.throwableShake;
+        }
+
+        if (this.inGround)
+        {
+            if (this.world.getBlockState(new BlockPos(this.xTile, this.yTile, this.zTile)).getBlock() == this.inTile)
+            {
+                ++this.ticksInGround;
+
+                if (this.ticksInGround == 1200)
+                {
+                    this.setDead();
+                }
+
+                return;
+            }
+
+            this.inGround = false;
+            this.motionX *= (double)(this.rand.nextFloat() * 0.2F);
+            this.motionY *= (double)(this.rand.nextFloat() * 0.2F);
+            this.motionZ *= (double)(this.rand.nextFloat() * 0.2F);
+            this.ticksInGround = 0;
+            this.ticksInAir = 0;
+        }
+        else
+        {
+            ++this.ticksInAir;
+        }
+
+        Vec3d vec3d = new Vec3d(this.posX, this.posY, this.posZ);
+        Vec3d vec3d1 = new Vec3d(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
+        RayTraceResult raytraceresult = this.world.rayTraceBlocks(vec3d, vec3d1);
+        vec3d = new Vec3d(this.posX, this.posY, this.posZ);
+        vec3d1 = new Vec3d(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
+
+        if (raytraceresult != null)
+        {
+            vec3d1 = new Vec3d(raytraceresult.hitVec.x, raytraceresult.hitVec.y, raytraceresult.hitVec.z);
+        }
+
+        Entity entity = null;
+        List<Entity> list = this.world.getEntitiesWithinAABBExcludingEntity(this, this.getEntityBoundingBox().expand(this.motionX, this.motionY, this.motionZ).grow(1.0D));
+        double d0 = 0.0D;
+        boolean flag = false;
+
+        for (int i = 0; i < list.size(); ++i)
+        {
+            Entity entity1 = list.get(i);
+
+            if (entity1.canBeCollidedWith())
+            {
+                if (entity1 == this.ignoreEntity)
+                {
+                    flag = true;
+                }
+                else if (this.thrower != null && this.ticksExisted < 2 && this.ignoreEntity == null)
+                {
+                    this.ignoreEntity = entity1;
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                    AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().grow(0.30000001192092896D);
+                    RayTraceResult raytraceresult1 = axisalignedbb.calculateIntercept(vec3d, vec3d1);
+
+                    if (raytraceresult1 != null)
+                    {
+                        double d1 = vec3d.squareDistanceTo(raytraceresult1.hitVec);
+
+                        if (d1 < d0 || d0 == 0.0D)
+                        {
+                            entity = entity1;
+                            d0 = d1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (entity != null)
+        {
+            raytraceresult = new RayTraceResult(entity);
+        }
+
+        if (raytraceresult != null)
+        {
+            if (raytraceresult.typeOfHit == RayTraceResult.Type.BLOCK && this.world.getBlockState(raytraceresult.getBlockPos()).getBlock() == Blocks.PORTAL)
+            {
+                this.setPortal(raytraceresult.getBlockPos());
+            }
+            else if (!net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, raytraceresult))
+            {
+                this.onImpact(raytraceresult);
+            }
+        }
+
+        this.posX += this.motionX;
+        this.posY += this.motionY;
+        this.posZ += this.motionZ;
+        float f = MathHelper.sqrt(this.motionX * this.motionX + this.motionZ * this.motionZ);
+        this.rotationYaw = (float)(MathHelper.atan2(this.motionX, this.motionZ) * (180D / Math.PI));
+
+        for (this.rotationPitch = (float)(MathHelper.atan2(this.motionY, (double)f) * (180D / Math.PI)); this.rotationPitch - this.prevRotationPitch < -180.0F; this.prevRotationPitch -= 360.0F)
+        {
+            ;
+        }
+
+        while (this.rotationPitch - this.prevRotationPitch >= 180.0F)
+        {
+            this.prevRotationPitch += 360.0F;
+        }
+
+        while (this.rotationYaw - this.prevRotationYaw < -180.0F)
+        {
+            this.prevRotationYaw -= 360.0F;
+        }
+
+        while (this.rotationYaw - this.prevRotationYaw >= 180.0F)
+        {
+            this.prevRotationYaw += 360.0F;
+        }
+
+        this.rotationPitch = this.prevRotationPitch + (this.rotationPitch - this.prevRotationPitch) * 0.2F;
+        this.rotationYaw = this.prevRotationYaw + (this.rotationYaw - this.prevRotationYaw) * 0.2F;
+        float f2 = this.getGravityVelocity();
+
+        if (this.isInWater())
+        {
+            this.world.spawnParticle(
+                    EnumParticleTypes.SMOKE_LARGE,
+                    this.posX,
+                    this.posY,
+                    this.posZ,
+                    0.0D,
+                    2.0D,
+                    0.0D,
+                    50
+            );
+
+            if(!this.world.isRemote)
+                this.setDead();
+        }
+
+        this.motionX *= (double)0.999F;
+        this.motionY *= (double)0.98F;
+        this.motionZ *= (double)0.999F;
+
+        if (!this.hasNoGravity())
+        {
+            this.motionY -= (double)f2;
+        }
+
+        this.setPosition(this.posX, this.posY, this.posZ);
+
+        this.world.spawnParticle(
+                EnumParticleTypes.LAVA,
+                this.posX,
+                this.posY,
+                this.posZ,
+                0.0D,
+                0.0D,
+                0.0D,
+                5
+        );
+    }
+
+    protected void onImpact(RayTraceResult result)
+    {
+        if (!this.world.isRemote)
+        {
+            this.world.createExplosion(null, result.hitVec.x, result.hitVec.y, result.hitVec.z, 5.0F, true);
+            this.world.setEntityState(this, (byte)3);
+            this.setDead();
+        }
+    }
+}
