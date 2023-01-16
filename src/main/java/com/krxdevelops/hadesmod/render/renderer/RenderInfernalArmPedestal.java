e2c441bc5c5fda626c209208724d7e4278db7242
KRXDevelops-Kaan <kaanuyumaz.developer@gmail.com>
2023-01-16 17:16:57 +0300

    Infernal Arm Pedestal Titanblood Infusing Render Test

--- src/main/java/com/krxdevelops/hadesmod/render/renderer/RenderInfernalArmPedestal.java
+++ src/main/java/com/krxdevelops/hadesmod/render/renderer/RenderInfernalArmPedestal.java
@@ -1,5 +1,7 @@
 package com.krxdevelops.hadesmod.render.renderer;
 
+import com.krxdevelops.hadesmod.HadesMod;
+import com.krxdevelops.hadesmod.init.ItemInit;
 import com.krxdevelops.hadesmod.items.ShieldOfChaos;
 import com.krxdevelops.hadesmod.tileEntities.TileEntityInfernalArmPedestal;
 import net.minecraft.client.Minecraft;
@@ -11,9 +13,12 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemRedstone;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
@@ -34,15 +39,30 @@ public class RenderInfernalArmPedestal extends TileEntitySpecialRenderer<TileEnt
 
             GlStateManager.pushMatrix();
             GlStateManager.translate(0.5F, 0.0F, 0.5F);
+
+            for (int i = 0; i < 3; i++)
+            {
+                GlStateManager.rotate((te.getWorld().getTotalWorldTime() + i * 120 + partialTicks) % 360, 0.0F, 1.0F, 0.0F);
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(0.5F, 0.3F, 0.5F);
+                GlStateManager.disableLighting();
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 160, 8);
+                Minecraft.getMinecraft().getRenderItem().renderItem(new ItemStack(ItemInit.titanblood, 1), ItemCameraTransforms.TransformType.GROUND);
+                GlStateManager.enableLighting();
+                GlStateManager.popMatrix();
+                GlStateManager.rotate(-((te.getWorld().getTotalWorldTime() + i * 120 + partialTicks) % 360), 0.0F, 1.0F, 0.0F);
+            }
+
             GlStateManager.rotate((te.getWorld().getTotalWorldTime() + partialTicks) % 360, 0.0F, 1.0F, 0.0F);
+
             if (stackOnEntity.getItem() instanceof ShieldOfChaos)
             {
                 GlStateManager.translate(0.0F, 0.5F, 0.0F);
                 GlStateManager.rotate(70F, 1.0F, 0.0F, 0.0F);
             }
+
             GlStateManager.disableLighting();
             OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 160, 8);
-            this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
             Minecraft.getMinecraft().getRenderItem().renderItem(stackOnEntity, ItemCameraTransforms.TransformType.GROUND);
             GlStateManager.enableLighting();
             GlStateManager.popMatrix();
